1dbeca225a41490b8ed01bc7e1498a4307835ebe
Marselliy <sariksariksarik@gmail.com>
2018-10-26 22:33:20 +0300

    Fixed crash when passing None as keypoints or bboxes

--- src/tf_imgaug/sequential.py
+++ src/tf_imgaug/sequential.py
@@ -8,6 +8,10 @@ class Sequential:
         self.n_augments = n_augments
 
     def __call__(self, images, keypoints=None, bboxes=None):
+        if keypoints is None:
+            keypoints = tf.zeros(tf.concat([tf.shape(images)[:1], [0, 2]], axis=0))
+        if bboxes is None:
+            bboxes = tf.zeros(tf.concat([tf.shape(images)[:1], [0, 4]], axis=0))
         random.seed(self.seed)
         res = (images, keypoints, bboxes)
         res = [tf.tile(e, tf.concat([[self.n_augments], tf.ones_like(tf.shape(e)[1:], dtype=tf.int32)], axis=0)) for e in res]
