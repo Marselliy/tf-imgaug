b8780455e50a84eb2e1d701d4906ef1e8529a907
Marselliy <sariksariksarik@gmail.com>
2018-10-27 15:38:48 +0300

    Changed range for salt and pepper

--- src/tf_imgaug/augments.py
+++ src/tf_imgaug/augments.py
@@ -369,22 +369,22 @@ class AbstractNoise(AbstractAugment):
 class Salt(AbstractNoise):
 
     def __init__(self, p=0):
-        super(Salt, self).__init__(noise_range=(196, 255), p=p, per_channel=False, coarse=True)
+        super(Salt, self).__init__(noise_range=(128, 255), p=p, per_channel=False, coarse=True)
 
 class CoarseSalt(AbstractNoise):
 
     def __init__(self, p=0, size_percent=0.01):
-        super(CoarseSalt, self).__init__(noise_range=(196, 255), p=p, per_channel=False, coarse=True, size_percent=size_percent)
+        super(CoarseSalt, self).__init__(noise_range=(128, 255), p=p, per_channel=False, coarse=True, size_percent=size_percent)
 
 class Pepper(AbstractNoise):
 
     def __init__(self, p=0):
-        super(Pepper, self).__init__(noise_range=(0, 32), p=p, per_channel=False)
+        super(Pepper, self).__init__(noise_range=(0, 128), p=p, per_channel=False)
 
 class CoarsePepper(AbstractNoise):
 
     def __init__(self, p=0, size_percent=0.01):
-        super(CoarsePepper, self).__init__(noise_range=(0, 32), p=p, per_channel=False, coarse=True, size_percent=size_percent)
+        super(CoarsePepper, self).__init__(noise_range=(0, 128), p=p, per_channel=False, coarse=True, size_percent=size_percent)
 
 class Dropout(AbstractNoise):
 
