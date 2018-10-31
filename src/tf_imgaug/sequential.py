3a3ee9116c967d84fc2ab6b36b2df9441cc32488
Alexander Susha <isushik94@gmail.com>
2018-10-31 17:56:44 +0300

    Implemented bboxes and keypoints formats supports. It's already tested.

--- src/tf_imgaug/sequential.py
+++ src/tf_imgaug/sequential.py
@@ -12,16 +12,28 @@ class Sequential:
         if keypoints is None:
             keypoints_none = True
             keypoints = tf.zeros(tf.concat([tf.shape(images)[:1], [0, 2]], axis=0))
+
         bboxes_none = False
         if bboxes is None:
             bboxes_none = True
             bboxes = tf.zeros(tf.concat([tf.shape(images)[:1], [0, 4]], axis=0))
+        
+        if isinstance(keypoints, tuple):
+            keypoints, keypoints_format = keypoints
+        else:
+            keypoints_format = 'xy'
+
+        if isinstance(bboxes, tuple):
+            bboxes, bboxes_format = bboxes
+        else:
+            bboxes_format = 'xyxy'
+        
         random.seed(self.seed)
         res = (images, keypoints, bboxes)
         res = [tf.tile(e, tf.concat([[self.n_augments], tf.ones_like(tf.shape(e)[1:], dtype=tf.int32)], axis=0)) for e in res]
         for aug in self.augments:
             aug._set_seed(random.randint(0, 2 ** 32))
-            res = aug(*res)
+            res = aug(*(res[0], (res[1], keypoints_format), (res[2], bboxes_format)))
 
         result = [res[0]]
         if not keypoints_none:
