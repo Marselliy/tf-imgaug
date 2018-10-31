a66fd0a00a4c04d89e59f717de7ae9b8a4f8e5e3
Marselliy <sariksariksarik@gmail.com>
2018-10-31 17:05:26 +0200

    Added force casting to float32 for more speed

--- src/tf_imgaug/sequential.py
+++ src/tf_imgaug/sequential.py
@@ -19,11 +19,17 @@ class Sequential:
                     bboxes_none = True
                     bboxes = tf.zeros(tf.concat([tf.shape(images)[:1], [0, 4]], axis=0))
                 res = (images, keypoints, bboxes)
-                res = [tf.tile(e, tf.concat([[self.n_augments], tf.ones_like(tf.shape(e)[1:], dtype=tf.int32)], axis=0)) for e in res]
+                res = tuple([tf.tile(e, tf.concat([[self.n_augments], tf.ones_like(tf.shape(e)[1:], dtype=tf.int32)], axis=0)) for e in res])
+
+            if images.dtype != tf.float32:
+                res = (tf.image.convert_image_dtype(res[0], tf.float32),) + res[1:]
 
             for aug in self.augments:
                 aug._set_seed(self.random.randint(0, 2 ** 32))
                 res = aug(*res)
+
+            if images.dtype != tf.float32:
+                res = (tf.image.convert_image_dtype(res[0], images.dtype),) + res[1:]
 
             result = [res[0]]
             if not keypoints_none:
